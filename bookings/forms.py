daa5fc9de06570694fd0a11b6d2ea84845828ca8
Takedown-code <gj079s7s@students.codeinstitute.net>
2025-05-13 14:03:06 +0200

    added party size and end time

--- bookings/forms.py
+++ bookings/forms.py
@@ -1,20 +1,16 @@
-from django import forms
-from .models import Booking
-
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ['user_name', 'table', 'start_time']
+        fields = ['user_name', 'party_size', 'table', 'start_time', 'end_time']
         labels = {
             'user_name': 'Name',
         }
         widgets = {
-            'start_time': forms.DateTimeInput(
-                attrs={'type': 'datetime-local'},
-                format='%Y-%m-%dT%H:%M'
-            ),
+            'start_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'),
+            'end_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}, format='%Y-%m-%dT%H:%M'),
         }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['start_time'].input_formats = ['%Y-%m-%dT%H:%M']
+        self.fields['end_time'].input_formats = ['%Y-%m-%dT%H:%M']
