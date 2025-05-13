64ed033ceff026adeab35d954e28dfa1b641ac5a
Takedown-code <gj079s7s@students.codeinstitute.net>
2025-05-13 12:39:10 +0200

    Fixed path for app

--- restaurant_booking/config/settings.py
+++ restaurant_booking/config/settings.py
@@ -20,7 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bookings',
+    'restaurant_booking.bookings',  # ✅ fixed path to app
 ]
 
 MIDDLEWARE = [
