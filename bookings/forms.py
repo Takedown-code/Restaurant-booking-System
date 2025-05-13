b3549336ec1a6bc1e160fbd73bfc21ca56edfeb0
Takedown-code <gj079s7s@students.codeinstitute.net>
2025-05-13 13:49:24 +0200

    solving issue when trying to book table

--- bookings/forms.py
+++ bookings/forms.py
@@ -1,11 +1,21 @@
+# bookings/forms.py
 from django import forms
 from .models import Booking
 
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ['user_name', 'party_size', 'table', 'start_time', 'end_time']
-        widgets = {
-            'start_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
-            'end_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        fields = ['user_name', 'table', 'start_time']
+        labels = {
+            'user_name': 'Name',  # changes label from 'User name' to 'Name'
         }
+        widgets = {
+            'start_time': forms.DateTimeInput(
+                attrs={'type': 'datetime-local'},
+                format='%Y-%m-%dT%H:%M'
+            ),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['start_time'].input_formats = ['%Y-%m-%dT%H:%M']
--- bookings/views.py
+++ bookings/views.py
@@ -1,24 +1,13 @@
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
-from .models import Booking
-
-class BookingListView(ListView):
-    model = Booking
-    template_name = 'bookings/booking_list.html'
+from .forms import BookingForm
 
 class BookingCreateView(CreateView):
     model = Booking
-    fields = ['user_name', 'table', 'start_time']
+    form_class = BookingForm
     template_name = 'bookings/booking_form.html'
     success_url = reverse_lazy('booking_list')
 
 class BookingUpdateView(UpdateView):
     model = Booking
-    fields = ['user_name', 'table', 'start_time']
+    form_class = BookingForm
     template_name = 'bookings/booking_form.html'
-    success_url = reverse_lazy('booking_list')
-
-class BookingDeleteView(DeleteView):
-    model = Booking
-    template_name = 'bookings/booking_confirm_delete.html'
     success_url = reverse_lazy('booking_list')
