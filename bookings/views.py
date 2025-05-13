f8657f1050e5ded7362d78ab7a0b9686faf2fd34
Takedown-code <gj079s7s@students.codeinstitute.net>
2025-05-13 13:52:51 +0200

    solving issue with render

--- bookings/views.py
+++ bookings/views.py
@@ -1,4 +1,11 @@
-from .forms import BookingForm
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Booking
+from .forms import BookingForm  # if you're using the custom form
+
+class BookingListView(ListView):
+    model = Booking
+    template_name = 'bookings/booking_list.html'
 
 class BookingCreateView(CreateView):
     model = Booking
@@ -10,4 +17,9 @@ class BookingUpdateView(UpdateView):
     model = Booking
     form_class = BookingForm
     template_name = 'bookings/booking_form.html'
+    success_url = reverse_lazy('booking_list')
+
+class BookingDeleteView(DeleteView):
+    model = Booking
+    template_name = 'bookings/booking_confirm_delete.html'
     success_url = reverse_lazy('booking_list')
