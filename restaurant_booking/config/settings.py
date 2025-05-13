a34fc0f8e2808198b5df8fd5620bc106ee8a5e4b
Takedown-code <gj079s7s@students.codeinstitute.net>
2025-05-13 12:48:31 +0200

    Fix TEMPLATES block syntax

--- restaurant_booking/config/settings.py
+++ restaurant_booking/config/settings.py
@@ -44,3 +44,29 @@ TEMPLATES = [
             ],
         },
     }
+]
+
+WSGI_APPLICATION = 'restaurant_booking.config.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
+STATIC_URL = 'static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
